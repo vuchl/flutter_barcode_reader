1417dc21c188e07e7d520c2b89f507ced689d58d
Sebastian <vuchl@users.noreply.github.com>
2018-11-06 12:42:47 +0100

    Add optional function argument

--- lib/barcode_scan.dart
+++ lib/barcode_scan.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:barcode_scan/scan_options.dart';
 
 class BarcodeScanner {
   static const CameraAccessDenied = 'PERMISSION_NOT_GRANTED';
   static const MethodChannel _channel =
       const MethodChannel('com.apptreesoftware.barcode_scan');
-  static Future<String> scan() async => await _channel.invokeMethod('scan');
+  static Future<String> scan({ScanOptions options}) async => await _channel.invokeMethod('scan');
 }
